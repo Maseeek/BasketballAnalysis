6f2ad128f0ab0353fa80a1d77d3b3c2ddc709d08
Maseeek <maseeekyt@gmail.com>
2024-07-31 23:29:14 +0100

    IM HIM EVERYTHING WORKS

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import numpy as np
 
 def findBasketballCenter(frame):
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurredFrame = cv2.GaussianBlur(grayFrame, (9, 9), 0)
+    blurredFrame = cv2.GaussianBlur(grayFrame, (15, 15), 0)
     edges = cv2.Canny(blurredFrame, 30, 100)
+    cv2.imshow('edges', edges)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     center = None
     radius = 0
@@ -30,21 +31,29 @@ def findBasketballCenter(frame):
 
     return center, radius
 
-image = cv2.imread('image5.jpg')
-findBasketballCenter(image)
 
 
-
-cap = cv2.VideoCapture('video.mp4')
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret:
-        center, radius = findBasketballCenter(frame)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+def traceContoursOnVideo(videoPath):
+    cap = cv2.VideoCapture(videoPath)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            center, radius = findBasketballCenter(frame)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+        else:
             break
-    else:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+#PATH = r'E:\Youtube\tiktoks\footage\day 95\PXL_20231016_155547429.TS.mp4'
+PATH = 'NEAVid2.mov'
+traceContoursOnVideo(PATH)
+
+# i want to add a thing which looks at the last position of the ball
+# using the last position of the ball it can choose the contour closest to it
+# and then it can find the new position of the ball
+# this way we can track the ball
+
