0013a293cfca6fc9aa73a25c8069d9bdc1ad7cd5
Maseeek <maseeekyt@gmail.com>
2024-07-31 23:15:46 +0100

    simplified and removed lastPosition

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import cv2
 import numpy as np
 
 
-def findBasketballCenter(frame, lastPosition):
+def findBasketballCenter(frame):
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurredFrame = cv2.GaussianBlur(grayFrame, (9, 9), 0)
     edges = cv2.Canny(blurredFrame, 30, 100)
-    cv2.imshow('edges', edges)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     center = None
     radius = 0
-    min_distance = float('inf')  # Initialize minimum distance as infinity
 
-    # find the closest circle to the last position
+    # find the largest circle
     for contour in contours:
         (x, y), r = cv2.minEnclosingCircle(contour)
         area_contour = cv2.contourArea(contour)
@@ -22,17 +20,9 @@ def findBasketballCenter(frame, lastPosition):
         shape_factor = area_contour / area_circle
 
         # if the shape factor is close to 1, it's likely to be a circle
-        if 0.8 < shape_factor < 1.2:
-            if lastPosition is not None:  # Check if lastPosition is not None
-                distance = np.sqrt((x - lastPosition[0])**2 + (y - lastPosition[1])**2)  # Calculate Euclidean distance
-                if distance < min_distance:  # If this circle is closer to the last position
-                    min_distance = distance
-                    radius = r
-                    center = (int(x), int(y))
-            else:  # If lastPosition is None, select the contour based on the shape factor and radius
-                if r > radius:
-                    radius = r
-                    center = (int(x), int(y))
+        if r > radius and 0.8 < shape_factor < 1.2:
+            radius = r
+            center = (int(x), int(y))
 
     # draw the circle
     if center is not None:
@@ -41,19 +31,15 @@ def findBasketballCenter(frame, lastPosition):
     return center, radius
 
 image = cv2.imread('image5.jpg')
-findBasketballCenter(image, None)
+findBasketballCenter(image)
 
 
 
 cap = cv2.VideoCapture('video.mp4')
-lastPosition = None
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret:
-
-        center, radius = findBasketballCenter(frame, lastPosition)
-        if(center is not None):
-            lastPosition = center
+        center, radius = findBasketballCenter(frame)
         cv2.imshow('frame', frame)
         if cv2.waitKey(100) & 0xFF == ord('q'):
             break
