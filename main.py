32bf168b22cf7ed793464c17c8b7ac3c0f05c17f
Maseeek <maseeekyt@gmail.com>
2024-09-04 18:59:06 +0100

    removed findBasketballCenter function

--- main.py
+++ main.py
@@ -6,34 +6,7 @@ import math
 
 dist = lambda x1, y1, x2, y2: (x1-x2)**2 + (y1-y2)**2
 
-def findBasketballCenter(frame):
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurredFrame = cv2.GaussianBlur(grayFrame, (17, 17), 0)
-    edges = cv2.Canny(blurredFrame, 30, 100)
-    #cv2.imshow('edges', edges)
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    center = None
-    radius = 0
 
-    # find the largest circle
-    for contour in contours:
-        (x, y), r = cv2.minEnclosingCircle(contour)
-        area_contour = cv2.contourArea(contour)
-        area_circle = np.pi * r * r
-
-        # shape factor
-
-        shape_factor = area_contour / area_circle
-        # if the shape factor is close to 1, it's likely to be a circle
-        if r > radius and 0.8 < shape_factor < 1.2:
-            radius = r
-            center = (int(x), int(y))
-
-    # draw the circle
-    if center is not None:
-        cv2.circle(frame, center, int(radius), (0, 255, 0), 2)
-
-    return center, radius
 
 def calculateAngle(positionListX, positionListY):
     # Calculate differences in coordinates
